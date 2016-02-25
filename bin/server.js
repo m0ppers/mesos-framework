96579c5cf562cdf402de7bbfd99bc011fa88c1b4
Andreas Streichardt <andreas@arangodb.com>
2016-02-25 15:08:49 +0100

    I DON'T WANT TO BE UNIVERSAL ISOMORPHIC AND IT IS OK!

--- bin/server.js
+++ bin/server.js
@@ -5,10 +5,11 @@ var rootDir = path.resolve(__dirname, '..');
 /**
  * Define isomorphic constants.
  */
-global.__CLIENT__ = false;
-global.__SERVER__ = true;
-global.__DISABLE_SSR__ = false;  // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
+global.__CLIENT__ = true;
+global.__SERVER__ = false;
+global.__DISABLE_SSR__ = true;  // <----- DISABLES SERVER SIDE RENDERING FOR ERROR DEBUGGING
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
+global.__DEVTOOLS__ = false;
 
 if (__DEVELOPMENT__) {
   if (!require('piping')({
--- src/server.js
+++ src/server.js
@@ -32,19 +32,6 @@ app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
 
 app.use(Express.static(path.join(__dirname, '..', 'static')));
 
-// Proxy to API server
-app.use('/api', (req, res) => {
-  proxy.web(req, res, {target: targetUrl});
-});
-
-app.use('/ws', (req, res) => {
-  proxy.web(req, res, {target: targetUrl + '/ws'});
-});
-
-server.on('upgrade', (req, socket, head) => {
-  proxy.ws(req, socket, head);
-});
-
 // added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
 proxy.on('error', (error, req, res) => {
   let json;
